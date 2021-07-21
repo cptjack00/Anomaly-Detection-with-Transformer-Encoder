8e5ddbe30b92ff0d67507c8d57aa5ae534f41587
FIL <godyudodis@gmail.com>
2021-07-22 06:31:45 +0700

    Solve train.py import error

--- code/train.py
+++ code/train.py
@@ -1,11 +1,10 @@
 import torch
-import torch
 from torch.utils.data.dataloader import DataLoader
 from data_loader import CustomDataset
 from model import make_model
 
 config = {'dataset': 'scada1_1', 'l_win': 500, 'pre_mask': 200, 'post_mask': 400,
-          'batch_size': 32, 'shuffle': True, 'dataloader_num_workers': 4, 'num_epoch': 20, 'model_path': '../models/'}
+          'batch_size': 32, 'shuffle': True, 'dataloader_num_workers': 4, 'num_epoch': 50, 'model_path': '../models/'}
 
 
 def create_dataloader(dataset, config):
@@ -32,7 +31,6 @@ def train_epoch(train_iter, model, criterion, opt, min_train_loss):
         opt.step()
         if i % 10 == 0:
             print(i, loss)
-        break
     if loss < min_train_loss:
         torch.save(model.state_dict(), config['model_path'] + f"best_train_{epoch}.pth")
         torch.save(opt.state_dict(), config['model_path'] + f"optimizer_{epoch}.pth")
@@ -41,13 +39,14 @@ def train_epoch(train_iter, model, criterion, opt, min_train_loss):
     if best_model != None:
         return best_model
 
-min_train_loss = float('inf')
-dataset = CustomDataset(config)
-dataloader = create_dataloader(dataset, config)
-model = make_model(
-    N=6, d_model=dataset.rolling_windows.shape[-1], l_win=config['l_win'], d_ff=128, h=1, dropout=0.1)
-model.float()
-model_opt = torch.optim.Adam(model.parameters())
-criterion = torch.nn.MSELoss()
-for epoch in range(config['num_epoch']):
-    train_epoch(dataloader, model, criterion, model_opt, min_train_loss)
+if __name__ == '__main__':
+    min_train_loss = float('inf')
+    dataset = CustomDataset(config)
+    dataloader = create_dataloader(dataset, config)
+    model = make_model(
+        N=6, d_model=dataset.rolling_windows.shape[-1], l_win=config['l_win'], d_ff=128, h=1, dropout=0.1)
+    model.float()
+    model_opt = torch.optim.Adam(model.parameters())
+    criterion = torch.nn.MSELoss()
+    for epoch in range(config['num_epoch']):
+        train_epoch(dataloader, model, criterion, model_opt, min_train_loss)
