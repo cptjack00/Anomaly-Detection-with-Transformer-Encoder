10e1695655b6354efa29806fac0f5c4d80b5b10d
Le Anh Quang <quangnb2000@gmail.com>
2021-07-21 08:09:32 +0700

    Add a DataLoader

--- code/train.py
+++ code/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch
 import numpy as np
+from torch.utils.data.dataloader import DataLoader
 from model import make_model
 from utils import NoamOpt, loss_backprop
 
@@ -9,4 +10,7 @@ def window_mask(size, config):
     attn_shape = (1, size, size)
     window_mask = np.ones(attn_shape).astype('uint8')
     window_mask[:, :, config['pre_mask']:config['post_mask']] = 0 
-    return torch.from_numpy(window_mask) == 0
+    return torch.from_numpy(window_mask) 
+
+def create_dataloader(dataset, config):
+    return DataLoader(dataset, batch_size=config['batch_size'], shuffle=config['shuffle'], num_workers=config['dataloader_num_workers'])
