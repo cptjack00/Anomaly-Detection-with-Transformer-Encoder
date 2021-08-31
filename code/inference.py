9b6db76fbb1f3b213d096945ceec7387bf20ebe9
cptjack00 <quangnb2000@gmail.com>
2021-08-31 13:22:03 +0000

    refactor

--- code/inference.py
+++ code/inference.py
@@ -12,57 +12,41 @@ from models import make_autoencoder_model, make_transformer_model
 from train import create_dataloader, create_mask
 from utils import get_args, process_config, save_config
 
-try:
-    args = get_args()
-    config = process_config(args.config)
-except:
-    print("Missing or invalid arguments")
-    exit(0)
-dataset = CustomDataset(config, train=False)
-data_loader = create_dataloader(dataset, config)
+def load_model(config):
+    autoencoder_model = make_autoencoder_model(in_seq_len=config["autoencoder_dims"],
+                                               out_seq_len=config["l_win"],
+                                               d_model=config["d_model"])
+    autoencoder_model.load_state_dict(torch.load(
+        config["checkpoint_dir"] + config["best_auto_model"]))
+    autoencoder_model.float()
+    autoencoder_model.eval()
 
-autoencoder_model = make_autoencoder_model(
-    seq_len=config["autoencoder_dims"], d_model=config["d_model"])
-autoencoder_model.load_state_dict(torch.load(
-    config["checkpoint_dir"] + config["best_auto_model"]))
-autoencoder_model.float()
-autoencoder_model.eval()
-encoder = autoencoder_model.encoder
+    trans_model = make_transformer_model(N=config["num_stacks"],
+                                         d_model=config["d_model"],
+                                         l_win=config["l_win"],
+                                         d_ff=config["d_ff"],
+                                         h=config["num_heads"],
+                                         dropout=config["dropout"])
+    trans_model.load_state_dict(torch.load(
+        config["checkpoint_dir"] + config["best_trans_model"]))
+    trans_model.float()
+    trans_model.eval()
 
-mask = create_mask(config)
-trans_model = make_transformer_model(N=config["num_stacks"], d_model=config["d_model"], l_win=config["l_win"],
-                                     d_ff=config["d_ff"], h=config["num_heads"], dropout=config["dropout"]).float()
-trans_model.load_state_dict(torch.load(
-    config["checkpoint_dir"] + config["best_trans_model"]))
-trans_model.eval()
+    return autoencoder_model.encoder, trans_model
 
-loss = torch.nn.MSELoss()
-n_test = dataset.rolling_windows.shape[0]
-recon_loss = np.zeros(n_test)
-for i, batch in enumerate(data_loader):
-    src = encoder(batch["input"].float())
-    trg = encoder(batch["target"].float())
-    out = trans_model(src, src_mask=mask)
-    for j in range(config["batch_size"]):
-        try:
-            recon_loss[i * config["batch_size"] + j] = loss(
-                out[j, config["pre_mask"]:config["post_mask"], :], trg[j, config["pre_mask"]:config["post_mask"], :])
-        except:
-            pass
-
-idx_anomaly_test = dataset.data["idx_anomaly_test"]
-anomaly_index = []
-test_labels = np.zeros(n_test)
-for i in range(len(idx_anomaly_test)):
-    idx_start = idx_anomaly_test[i] - (config["l_win"] + 1)
-    idx_end = idx_anomaly_test[i] + (config["l_win"] + 1)
-    if idx_start < 0:
-        idx_start = 0
-    if idx_end > n_test:
-        idx_end = n_test
-    anomaly_index.append(np.arange(idx_start, idx_end))
-    test_labels[idx_start:idx_end] = 1
-
+def create_labels(idx_anomaly_test, n_test, config):
+    anomaly_index = []
+    test_labels = np.zeros(n_test)
+    for i in range(len(idx_anomaly_test)):
+        idx_start = idx_anomaly_test[i] - (config["l_win"] + 1)
+        idx_end = idx_anomaly_test[i] + (config["l_win"] + 1)
+        if idx_start < 0:
+            idx_start = 0
+        if idx_end > n_test:
+            idx_end = n_test
+        anomaly_index.append(np.arange(idx_start, idx_end))
+        test_labels[idx_start:idx_end] = 1
+    return anomaly_index, test_labels
 
 def return_anomaly_idx_by_threshold(test_anomaly_metric, threshold):
     # test_list = np.squeeze(np.ndarray.flatten(test_anomaly_metric))
@@ -72,7 +56,6 @@ def return_anomaly_idx_by_threshold(test_anomaly_metric, threshold):
         idx_error = np.expand_dims(idx_error, 0)
 
     return list(idx_error)
-
 
 def augment_detected_idx(idx_detected_anomaly, anomaly_index):
     n_anomaly = len(anomaly_index)
@@ -87,9 +70,7 @@ def augment_detected_idx(idx_detected_anomaly, anomaly_index):
                     list(currect_anomaly_win - in_original_detection)
                 # print(j)
                 break
-
     return list(np.sort(idx_detected_anomaly_extended))
-
 
 def count_TP_FP_FN(idx_detected_anomaly, test_labels):
     n_TP = 0
@@ -112,7 +93,6 @@ def count_TP_FP_FN(idx_detected_anomaly, test_labels):
     n_TN = len(test_labels) - n_TP - n_FP - n_FN
     return n_TP, n_FP, n_FN, n_TN
 
-
 def compute_precision_and_recall(idx_detected_anomaly, test_labels):
     # compute true positive
     n_TP, n_FP, n_FN, n_TN = count_TP_FP_FN(idx_detected_anomaly, test_labels)
@@ -130,10 +110,7 @@ def compute_precision_and_recall(idx_detected_anomaly, test_labels):
 
     return precision, recall, F1, fpr, n_TP, n_FP, n_FN
 
-
 def KQp(data, q):
-    # UNTITLED3 Summary of this function goes here
-    #   Detailed explanation goes here
     data2 = np.sort(data)  # sap xep tang dan
     n = np.shape(data2)[0]  # kich thuoc
     p = 1-q  # q tu xet, dat bang smth 0.05 0.025 0.01
@@ -147,95 +124,140 @@ def KQp(data, q):
     #KQp = KQ;
     return KQ
 
+def plot_roc_curve(fpr_aug, recall_aug, config, n_threshold=20):
+    tpr = np.insert(recall_aug, [0, n_threshold], [0, 1])
+    fpr = np.insert(fpr_aug, [0, n_threshold], [0, 1])
+    auc = metrics.auc(fpr, tpr)
+    print("AUC =", auc)
+    lw = 2
+    plt.plot(fpr, tpr, color="darkorange", lw=lw,
+             label="ROC curve (area = %0.4f)" % auc)
+    plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Augmented Receiver operating characteristic of " +
+              config["auto_dataset"])
+    plt.legend(loc="lower right")
+    plt.savefig(config["result_dir"] + "augmentedroc.pdf")
+    return auc
 
-# for i in [0.99, 0.95, 0.9, 0.85, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]:
-#     print("KQp is:", KQp(recon_loss, i), "with q =", i)
+def select_threshold(recon_loss, anomaly_index, test_labels, config, n_threshold=20):
+    """
+    Select best threshold based on best F1-score
+    """
+    precision = np.zeros(n_threshold)
+    recall = np.zeros(n_threshold)
+    F1 = np.zeros(n_threshold)
+    precision_aug = np.zeros(n_threshold)
+    recall_aug = np.zeros(n_threshold)
+    F1_aug = np.zeros(n_threshold)
+    fpr_aug = np.zeros(n_threshold)
+    i = 0
+    threshold_list = np.linspace(np.amin(recon_loss), np.amax(
+        recon_loss), n_threshold, endpoint=False)
+    threshold_list = np.flip(threshold_list)
 
-n_threshold = 20
-precision = np.zeros(n_threshold)
-recall = np.zeros(n_threshold)
-F1 = np.zeros(n_threshold)
-precision_aug = np.zeros(n_threshold)
-recall_aug = np.zeros(n_threshold)
-F1_aug = np.zeros(n_threshold)
-fpr_aug = np.zeros(n_threshold)
-i = 0
-threshold_list = np.linspace(np.amin(recon_loss), np.amax(
-    recon_loss), n_threshold, endpoint=False)
-threshold_list = np.flip(threshold_list)
-for threshold in threshold_list:
-    # print(threshold_list[i])
-    idx_detection = return_anomaly_idx_by_threshold(recon_loss, threshold)
-    # augment the detection using the ground truth labels
-    # a method to discount the factor one anomaly appears in multiple consecutive windows
-    # introduced in "Unsupervised anomaly detection via variational auto-encoder for seasonal kpis in web applications"
-    idx_detection_augmented = augment_detected_idx(
-        idx_detection, anomaly_index)
-    precision_aug[i], recall_aug[i], F1_aug[i], fpr_aug[i], _, _, _ = compute_precision_and_recall(idx_detection_augmented,
-                                                                                                   test_labels)
-    i = i + 1
-    #print(precision, recall, F1)
+    for threshold in threshold_list:
+        # print(threshold_list[i])
+        idx_detection = return_anomaly_idx_by_threshold(recon_loss, threshold)
+        # augment the detection using the ground truth labels
+        # a method to discount the factor one anomaly appears in multiple consecutive windows
+        # introduced in "Unsupervised anomaly detection via variational auto-encoder for seasonal kpis in web applications"
+        idx_detection_augmented = augment_detected_idx(
+            idx_detection, anomaly_index)
+        precision_aug[i], recall_aug[i], F1_aug[i], fpr_aug[i], _, _, _ = compute_precision_and_recall(idx_detection_augmented,
+                                                                                                       test_labels)
+        i = i + 1
+        #print(precision, recall, F1)
 
+    auc = plot_roc_curve(fpr_aug, recall_aug, config)
 
-print("\nAugmented detection:")
-print("Best F1 score is {}".format(np.amax(F1_aug)))
-idx_best_threshold = np.squeeze(np.argwhere(F1_aug == np.amax(F1_aug)))
-print("Best threshold is {}".format(threshold_list[idx_best_threshold]))
-best_thres = np.min(threshold_list[idx_best_threshold])
-print("At this threshold, precision is {}, recall is {}".format(precision_aug[idx_best_threshold],
-                                                                recall_aug[idx_best_threshold]))
+    print("\nAugmented detection:")
+    print("Best F1 score is {}".format(np.amax(F1_aug)))
+    idx_best_threshold = np.squeeze(np.argwhere(F1_aug == np.amax(F1_aug)))
+    print("Best threshold is {}".format(threshold_list[idx_best_threshold]))
+    best_thres = np.min(threshold_list[idx_best_threshold])
+    print("At this threshold, precision is {}, recall is {}".format(precision_aug[idx_best_threshold],
+                                                                    recall_aug[idx_best_threshold]))
+    return best_thres, auc
 
-# Now select a threshold
-threshold = best_thres
-q_list = [0.99, 0.9, 0.1]
-temp = math.inf
-q_best = 0
-closest_thres = 0
-for q in q_list:
-    temp_thres = KQp(recon_loss, q)
-    #print(temp_thres,abs(temp_thres - threshold))
-    if abs(temp_thres - threshold) < temp:
-        temp = abs(temp_thres - threshold)
-        q_best = q
-        KQp_thres = temp_thres
-start = time.time()
+def select_KQp_threshold(threshold, recon_loss):
+    q_list = [0.99, 0.9, 0.1]
+    temp = math.inf
+    q_best = 0
+    closest_thres = 0
+    for q in q_list:
+        temp_thres = KQp(recon_loss, q)
+        #print(temp_thres,abs(temp_thres - threshold))
+        if abs(temp_thres - threshold) < temp:
+            temp = abs(temp_thres - threshold)
+            q_best = q
+            KQp_thres = temp_thres
+    print("Closest KQp threshold is {} at q = {}".format(KQp_thres, q_best))
+    return KQp_thres, q_best
 
-print("Closest KQp threshold is {} at q = {}".format(KQp_thres, q_best))
-config["q_best"] = q_best
-idx_detection = return_anomaly_idx_by_threshold(recon_loss, KQp_thres)
-# print(idx_detection)
-idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index)
-# print(anomaly_index_lstm)
-# print(idx_detection_augmented)
-precision, recall, F1, _, n_TP, n_FP, n_FN = compute_precision_and_recall(idx_detection_augmented,
-                                                                          test_labels)
-# print("\nPR evaluation using KQE:")
-# print("Precision: {}".format(precision))
-config["precision"] = precision
-# print("Recall: {}".format(recall))
-config["recall"] = recall
-# print("F1: {}".format(F1))
-config["F1"] = F1
-# print("TP: {}".format(n_TP))
-# print("FP: {}".format(n_FP))
-# print("FN: {}".format(n_FN))
+def main():
+    try:
+        args = get_args()
+        config = process_config(args.config)
+    except:
+        print("Missing or invalid arguments")
+        exit(0)
+    dataset = CustomDataset(config, train=False)
+    data_loader = create_dataloader(dataset, config)
+    encoder, trans_model = load_model(config)
+    mask = create_mask(config)
+    loss = torch.nn.MSELoss()
+    n_test = len(dataset)
+    recon_loss = np.zeros(n_test)
 
-tpr = np.insert(recall_aug, [0, n_threshold], [0, 1])
-fpr = np.insert(fpr_aug, [0, n_threshold], [0, 1])
-auc = metrics.auc(fpr, tpr)
-print("AUC =", auc)
-config["AUC"] = auc
-lw = 2
-plt.plot(fpr, tpr, color="darkorange", lw=lw,
-         label="ROC curve (area = %0.4f)" % auc)
-plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("Augmented Receiver operating characteristic of " +
-          config["auto_dataset"])
-plt.legend(loc="lower right")
-plt.savefig(config["result_dir"] + "augmentedroc.pdf")
-config["inference_time"] = (time.time() - start) / 60
-save_config(config)
+    start = time.time()
+    for i, batch in enumerate(data_loader):
+        src = encoder(batch["input"].float())
+        trg = encoder(batch["target"].float())
+        out = trans_model(src, src_mask=mask)
+        for j in range(config["batch_size"]):
+            try:
+                recon_loss[i * config["batch_size"] + j] = loss(
+                    out[j, config["pre_mask"]:config["post_mask"], :], trg[j, config["pre_mask"]:config["post_mask"], :])
+            except:
+                pass
+
+    idx_anomaly_test = dataset.data["idx_anomaly_test"]
+    anomaly_index, test_labels = create_labels(idx_anomaly_test,
+                                               n_test,
+                                               config)
+
+    # Now select a threshold
+    threshold, auc = select_threshold(recon_loss,
+                                      anomaly_index,
+                                      test_labels,
+                                      config)
+    config["AUC"] = auc
+
+    KQp_thres, q_best = select_KQp_threshold(threshold, recon_loss)
+    config["q_best"] = q_best
+    idx_detection = return_anomaly_idx_by_threshold(recon_loss, KQp_thres)
+    # print(idx_detection)
+    idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index)
+    # print(anomaly_index_lstm)
+    print(idx_detection_augmented)
+    precision, recall, F1, _, n_TP, n_FP, n_FN = compute_precision_and_recall(idx_detection_augmented,
+                                                                              test_labels)
+    config["precision"] = precision
+    config["recall"] = recall
+    config["F1"] = F1
+    config["inference_time"] = (time.time() - start) / 60
+    save_config(config)
+    # print("\nPR evaluation using KQE:")
+    # print("Precision: {}".format(precision))
+    # print("Recall: {}".format(recall))
+    # print("F1: {}".format(F1))
+    # print("TP: {}".format(n_TP))
+    # print("FP: {}".format(n_FP))
+    # print("FN: {}".format(n_FN))
+
+if __name__ == '__main__':
+    main()
