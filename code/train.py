21828f20abc744ca44f0a36b00fe2bd5f5b28176
cptjack00 <quangnb2000@gmail.com>
2021-08-31 13:23:11 +0000

    Add logging and change best_auto_model def

--- code/train.py
+++ code/train.py
@@ -1,7 +1,7 @@
 import time
 
 import torch
-from torch.utils.data.dataloader import DataLoader, T
+from torch.utils.data.dataloader import DataLoader
 
 from data_loader import CustomDataset
 from models import make_autoencoder_model, make_transformer_model
@@ -10,7 +10,10 @@ from utils import create_dirs, get_args, process_config, save_config
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def create_dataloader(dataset, config):
-    return DataLoader(dataset, batch_size=config["batch_size"], shuffle=bool(config["shuffle"]), num_workers=config["dataloader_num_workers"])
+    return DataLoader(dataset,
+                      batch_size=config["batch_size"],
+                      shuffle=bool(config["shuffle"]),
+                      num_workers=config["dataloader_num_workers"])
 
 
 def loss_backprop(criterion, out, targets):
@@ -31,15 +34,16 @@ def create_mask(config):
 
 min_trans_loss = float("inf")
 best_trans_model = None
-
+epoch_trans_loss = list()
 
 def trans_train_epoch(train_iter, model, autoencoder, criterion, mask, opt, epoch, config):
-    global min_trans_loss, best_trans_model
+    global min_trans_loss, best_trans_model, epoch_trans_loss
     model.train()
     model.to(device)
     autoencoder.eval()
     encoder = autoencoder.encoder
     encoder.to(device)
+    batch_loss = list()
     for i, batch in enumerate(train_iter):
         src = batch["input"].float()
         src.to(device)
@@ -52,14 +56,19 @@ def trans_train_epoch(train_iter, model, autoencoder, criterion, mask, opt, epoc
         opt.zero_grad()
         loss = loss_backprop(criterion, out, trg)
         opt.step()
-        if i % 10 == 0:
-            print(i, loss)
-    if loss < min_trans_loss:
+        batch_loss.append(loss.item())
+
+    if len(batch_loss) > 0:
+        epoch_trans_loss.append(sum(batch_loss)/len(batch_loss))
+        logging.info('TRANSFORMER. Epoch: {} \tTotal Loss: {:.6f}'.format(epoch,
+                                                                          epoch_trans_loss[-1]))
+
+    if epoch_trans_loss[-1] < min_trans_loss:
         torch.save(model.state_dict(),
                    config["checkpoint_dir"] + f"best_trans_{epoch}.pth")
         torch.save(opt.state_dict(),
                    config["checkpoint_dir"] + f"optimizer_trans_{epoch}.pth")
-        min_trans_loss = loss
+        min_trans_loss = epoch_trans_loss[-1]
         best_trans_model = f"best_trans_{epoch}.pth"
     if best_trans_model != None:
         return best_trans_model
@@ -67,12 +76,13 @@ def trans_train_epoch(train_iter, model, autoencoder, criterion, mask, opt, epoc
 
 min_auto_loss = float("inf")
 best_auto_model = None
-
+epoch_auto_loss = list()
 
 def autoencoder_train_epoch(train_iter, model, criterion, opt, epoch, config):
-    global min_auto_loss, best_auto_model
+    global min_auto_loss, best_auto_model, epoch_auto_loss
     model.train()
     model.to(device)
+    batch_loss = list()
     for i, batch in enumerate(train_iter):
         src = batch["input"].float()
         src.to(device)
@@ -82,14 +92,19 @@ def autoencoder_train_epoch(train_iter, model, criterion, opt, epoch, config):
         opt.zero_grad()
         loss = loss_backprop(criterion, out, trg)
         opt.step()
-        if i % 10 == 0:
-            print(i, loss)
-    if loss < min_auto_loss:
+        batch_loss.append(loss.item())
+
+    if len(batch_loss) > 0:
+        epoch_auto_loss.append(sum(batch_loss)/len(batch_loss))
+        logging.info('AUTOENCODER. Epoch: {} \tTotal Loss: {:.6f}'.format(epoch,
+                                                                          epoch_auto_loss[-1]))
+
+    if epoch_auto_loss[-1] < min_auto_loss:
         torch.save(model.state_dict(),
                    config["checkpoint_dir"] + f"best_autoencoder_{epoch}.pth")
         torch.save(opt.state_dict(),
                    config["checkpoint_dir"] + f"optimizer_autoencoder_{epoch}.pth")
-        min_auto_loss = loss
+        min_auto_loss = epoch_auto_loss[-1]
         best_auto_model = f"best_autoencoder_{epoch}.pth"
     if best_auto_model != None:
         return best_auto_model
@@ -109,34 +124,49 @@ def main():
     dataset = CustomDataset(config)
     dataloader = create_dataloader(dataset, config)
 
-    start = time.time()
     # Training the autoencoder
-    autoencoder_model = make_autoencoder_model(
-        seq_len=config["autoencoder_dims"], d_model=config["d_model"])
+    start = time.time()
+    autoencoder_model = make_autoencoder_model(in_seq_len=config["autoencoder_dims"],
+                                               out_seq_len=config["l_win"],
+                                               d_model=config["d_model"])
     autoencoder_model.float()
     model_opt = torch.optim.Adam(autoencoder_model.parameters())
     criterion = torch.nn.MSELoss()
     for epoch in range(config["auto_num_epoch"]):
-        config["best_auto_model"] = autoencoder_train_epoch(
-            dataloader, autoencoder_model, criterion, model_opt, epoch, config)
+        config["best_auto_model"] = autoencoder_train_epoch(dataloader,
+                                                            autoencoder_model,
+                                                            criterion,
+                                                            model_opt,
+                                                            epoch,
+                                                            config)
     print("COMPLETED TRAINING THE AUTOENCODER")
     config["auto_train_time"] = (time.time() - start) / 60
-    start = time.time()
+
     # Training the transformer model
+    start = time.time()
     mask = create_mask(config)
-    trans_model = make_transformer_model(
-        N=config["num_stacks"], d_model=dataset.rolling_windows.shape[-1], l_win=config["l_win"], d_ff=128, h=1, dropout=0.1)
+    trans_model = make_transformer_model(N=config["num_stacks"],
+                                         d_model=len(dataset),
+                                         l_win=config["l_win"],
+                                         d_ff=128,
+                                         h=1,
+                                         dropout=0.1)
     trans_model.float()
     model_opt = torch.optim.Adam(trans_model.parameters())
     autoencoder_model.load_state_dict(
         torch.load(config["checkpoint_dir"] + config["best_auto_model"]))
     for epoch in range(config["trans_num_epoch"]):
-        config["best_trans_model"] = trans_train_epoch(
-            dataloader, trans_model, autoencoder_model, criterion, mask, model_opt, epoch, config)
+        config["best_trans_model"] = trans_train_epoch(dataloader,
+                                                       trans_model,
+                                                       autoencoder_model,
+                                                       criterion,
+                                                       mask,
+                                                       model_opt,
+                                                       epoch,
+                                                       config)
     print("COMPLETED TRAINING THE TRANSFORMER")
     config["trans_train_time"] = (time.time() - start) / 60
     save_config(config)
-
 
 if __name__ == "__main__":
     main()
