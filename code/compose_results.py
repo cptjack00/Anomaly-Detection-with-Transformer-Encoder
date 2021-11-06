333f4cde65ca8b3dc73a928c2de9eb2e66109f4e
cptjack00 <godyudodis@gmail.com>
2021-11-06 21:37:13 +0700

    Compose VANILLA results

--- code/compose_results.py
+++ code/compose_results.py
@@ -4,14 +4,17 @@ import json
 import csv
 
 csv_file = "result.csv"
-exps = os.listdir("../experiments")
+exps = os.listdir("../experiments/Vanilla Transformer/")
 count = 0
 res = []
 for exp in exps:
-    exp = os.path.join("../experiments", exp)
+    exp = os.path.join("../experiments/Vanilla Transformer/", exp)
     exp = os.path.join(exp, os.listdir(exp)[0], 'result')
     listdir = os.listdir(exp)
-    listdir.remove("augmentedroc.pdf")
+    try:
+        listdir.remove("augmentedroc.pdf")
+    except:
+        pass
     filename = os.path.join(exp, listdir[0])
     try:
         with open(filename) as f:
@@ -21,5 +24,5 @@ for exp in exps:
     except Exception as ex:
         print("EXCEPTION: ", filename)
 df = pd.DataFrame(res)
-df.to_csv("result.csv")
+df.to_csv(csv_file)
 
