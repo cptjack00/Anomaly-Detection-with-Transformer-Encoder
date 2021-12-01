bf25b795ca088908e890dba0cac2b524a32e2881
cptjack00 <godyudodis@gmail.com>
2021-12-02 00:16:52 +0700

    Fix positional encoding odd d_model bug

--- code/models.py
+++ code/models.py
@@ -150,12 +150,18 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
+        position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2)
                              * (-math.log(10000.0) / d_model))
 
-        pe[:, 0::2] = torch.sin(div_term * position)
-        pe[:, 1::2] = torch.cos(div_term * position)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        try:
+            pe[:, 1::2] = torch.cos(position * div_term)
+        except:
+            div_term = torch.exp(torch.arange(1, d_model, 2)
+                                 * (-math.log(10000.0) / d_model))
+            pe[:, 1::2] = torch.cos(position * div_term)
+
         pe = pe.unsqueeze(0)
         self.register_buffer("pe", pe)
 
